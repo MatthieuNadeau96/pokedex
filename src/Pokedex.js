8f376f96186bc47887a7483327b1191762b7eee0
Matthieu <matthieunadeau96@gmail.com>
2019-05-14 16:11:51 -0400

    Styled Pokecard and Pokedex components

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Pokecard from './Pokecard'
+import './Pokedex.css'
 
 class Pokedex extends Component {
 
@@ -22,9 +23,11 @@ class Pokedex extends Component {
     return (
       <div>
         <h1>Pokedex!</h1>
-        {this.props.pokemon.map((p) => (
-          <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
-        ))}
+        <div className="Pokedex-cards">
+          {this.props.pokemon.map((p) => (
+            <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
+          ))}
+        </div>
       </div>
     );
   }
